7490315099b55d72304cbc4583472abd88896b7c
Loris Tissino <loris.tissino@gmail.com>
2017-08-26 17:59:45 +0200

    Fix: MediaStreamTrack.getSources() is deprecated; replaced with navigator.mediaDevices.enumerateDevices()

--- threex.webcamgrabbing.js
+++ threex.webcamgrabbing.js
@@ -77,7 +77,7 @@ THREEx.WebcamGrabbing = function(){
         }, 500)
 
         // get the media sources
-        MediaStreamTrack.getSources(function(sourceInfos) {
+        navigator.mediaDevices.enumerateDevices().then(function(sourceInfos) {
                 // define getUserMedia() constraints
                 var constraints = {
                         video: true,
